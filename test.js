f23c14cb2253179e78e81e35f5219035c094a749
franck-artaud <franck-artaud@atelier601.com>
2019-04-19 16:45:58 +0200

    cotéserveur

--- test.js
+++ test.js
@@ -1,49 +1,41 @@
 const request = require('request');
 const cheerio = require('cheerio');
 const fs = require('fs');
-const doc = fs.createWriteStream('résulta.csv');
+const doc = fs.createWriteStream('vente.csv');
 
-doc.write(`Profession,entreprise,lieux,description  \r`);
-
-
-// var url = document.getElementById("url").value;
+doc.write(`Profession,entreprise,lieux,description  \r`)
 
 
-//function req () {
+request('https://www.indeed.fr/emplois?q=vendeuse&l=Vend%C3%A9e', (error, response, html) => {
 
-   // var url = document.getElementById("url").value;
+if (!error && response.statusCode == 200){
+    const $ = cheerio.load(html);
 
-    request("https://www.indeed.fr/emplois?q=developpeur&l=Vend%C3%A9e", (error, response, html) => {
+   $('.jobsearch-SerpJobCard').each((i, el) => {
+       const Profession = $(el)
+       .find('.title')
+       .text()
+        .replace(/\s\s+/g,'');
+        const entreprise = $(el)
+        .find('.company')
+        .text()
+        .replace(/\s\s+/g,'');
+        const lieux = $(el)
+        .find('.location')
+        .text()
+        .replace(/\s\s+/g,'');
+        const description = $(el)
+        .find('.summary')
+        .text()
+        .replace(/\s\s+/g,'');
 
-        if (!error && response.statusCode == 200){
-            const $ = cheerio.load(html);
-        
-           $('.jobsearch-SerpJobCard').each((i, el) => {
-               const Profession = $(el)
-               .find('.title')
-               .text()
-                .replace(/\s\s+/g,'');
-                const entreprise = $(el)
-                .find('.company')
-                .text()
-                .replace(/\s\s+/g,'');
-                const lieux = $(el)
-                .find('.location')
-                .text()
-                .replace(/\s\s+/g,'');
-                const description = $(el)
-                .find('.summary')
-                .text()
-                .replace(/\s\s+/g,'');
-        
-                doc.write(`${Profession},${entreprise},${lieux},${description} \r`);
-              
-        
-                console.log(Profession+'  '+'  '+entreprise+'  '+' '+lieux+'  '+' '+description);
-                 
-               
-               
-           });
-        }
-        });
-//}
+        doc.write(`${Profession},${entreprise},${lieux},${description} \r`);
+      
+
+        console.log(Profession+'  '+'  '+entreprise+'  '+' '+lieux+'  '+' '+description);
+         
+       
+       
+   });
+}
+});
